9899df34bff27df5fff22485d50eb45b8030ded2
Animesh Nepal <aneemes1@gmail.com>
2022-05-13 19:40:15 +0545

    sprint-01-release-register-login

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,9 +1,40 @@
 import React from "react";
 import {Col, Container, Row, Form, Button} from "react-bootstrap"
-import {Link} from "react-router-dom"
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import re from "../../images/re.png"
 import './signup.css'
 const SignUp = () =>{
+    const [credentials, setCredentinals] = useState({
+        username: undefined,
+        email:undefined,
+        password: undefined,
+    })
+
+    const navigate = useNavigate()
+
+    const { user, loading, error, dispatch } = useContext(AuthContext);
+
+    const handleChange = (e)=>{
+        setCredentinals(prev=>({...prev, [e.target.id]: e.target.value }))
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "REGISTER_START" });
+        try {
+          const res = await axios.post("http://localhost:8000/api/auth/register/", credentials);
+          dispatch({ type: "REGISTER_SUCCESS", payload: res.data.details });
+          navigate("/")
+        } catch (err) {
+          dispatch({ type: "REGISTER_FAILURE", payload: err.response.data });
+        }
+    };
+
+    console.log(user);
+
     return<div>
         <Container className ="mt-5">
             <Row>
@@ -13,18 +44,18 @@ const SignUp = () =>{
                     <h1 className="shadow-sm text  mt-5 p-3 text-center rounded">User Register</h1>
                         
                     <Form.Group controlId="formBasicEmail" >
-                            <Form.Label>Full Name:</Form.Label>                       
-                            <Form.Control type = "text" placeholder="Enter your Fullname" />
+                            <Form.Label>Username:</Form.Label>                       
+                            <Form.Control type = "text" placeholder="Enter your username" id="username" onChange={handleChange}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicEmail" className="mt-3" >
                             <Form.Label>Email:</Form.Label> 
-                            <Form.Control type = "email" placeholder="Enter your email" />
+                            <Form.Control type = "email" placeholder="Enter your email" id="email" onChange={handleChange}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword" className="mt-3">
                             <Form.Label>Password:</Form.Label> 
-                            <Form.Control type = "password" placeholder="Enter your Password" />
+                            <Form.Control type = "password" placeholder="Enter your Password" id="password" onChange={handleChange}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword" className="mt-3">
@@ -32,7 +63,7 @@ const SignUp = () =>{
                             <Form.Control type = "password" placeholder="Enter your Password Again " />
                         </Form.Group>
 
-                        <Button variant=" button  mt-3 center" type ="submit">Proceed</Button>
+                        <Button disabled={loading} onClick={handleClick} variant=" button  mt-3 center" type ="submit">Proceed</Button>
 
                          <Link to ="/"> Back to Login page </Link>
 
